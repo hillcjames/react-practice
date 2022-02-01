6ae7884333def42be5321c3032b60b6502c19945
Chris Hill <christopher.hill@caci.com>
2022-02-01 17:04:09 -0500

    UNSAFE TESTING display parameter

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # This is the exact opposite of safe. The textbook definition of an injection vulnerability, I think. Just here for testing.
+    arg = request.args.get('username')
+    return "<p>Hello, World!" + arg + "</p>"
